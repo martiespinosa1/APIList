8e822330f214b2b8832fd46c12be901e6f0bfa1c
marti <martiespinosa99@gmail.com>
2024-03-01 21:54:10 +0100

    ya

--- app/src/main/java/com/example/apilist/view/List.kt
+++ app/src/main/java/com/example/apilist/view/List.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -120,13 +122,17 @@ fun CharacterItem(character: Data, navController: NavController, myAPIViewModel:
         Box(
             modifier = Modifier.background(Color.Gray).fillMaxWidth()
         ) {
-            Row(modifier = Modifier.padding(16.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 16.dp, end = 16.dp, bottom = 16.dp)
+            ) {
                 GlideImage(
                     model = character.images.small,
                     contentDescription = "Card Image",
                     contentScale = ContentScale.FillHeight,
                     modifier = Modifier.size(125.dp)
                 )
+                Spacer(modifier = Modifier.width(16.dp))
                 Text(
                     text = character.name,
                     color = Color.LightGray,
@@ -134,13 +140,6 @@ fun CharacterItem(character: Data, navController: NavController, myAPIViewModel:
                     fontWeight = FontWeight.Bold,
                     fontFamily = FontFamily.Monospace
                 )
-//                Text(
-//                    text = character.types?.get(0) ?: "",
-//                    color = Color.LightGray,
-//                    fontSize = 18.sp,
-//                    fontWeight = FontWeight.Bold,
-//                    fontFamily = FontFamily.Monospace
-//                )
             }
         }
     }
